e8f7db6e7472d6a700916c64d7ae45b5435c3986
kryst <kryst@DESKTOP-05F91LJ>
2022-07-10 22:02:08 +0200

    Dodaj pliki projektów.

---
+++ TimeCounter2022/Entities/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCounter2022.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string NameTask { get; set; }
+        public DateTime CurrentTime { get; set; }
+        public DateTime TotalTime { get; set; }
+
+        public string GetAllTime
+        {
+            get 
+            { 
+                return $"{CurrentTime} {TotalTime}".ToString(); 
+            }
+        }
+    }
+}
---
+++ TimeCounter2022/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeCounter2022
+{
+    using TimeCounter2022.Entities;
+
+    
+    public partial class MainWindow : Window
+    {
+        public List<Task> tasks = new List<Task>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tasks.Add(new Task { NameTask = "zadanie 1", TotalTime = DateTime.MinValue, CurrentTime = DateTime.Now });
+            tasks.Add(new Task { NameTask = "zadanie 2", TotalTime = DateTime.MinValue, CurrentTime = DateTime.Now });
+            tasks.Add(new Task { NameTask = "zadanie 1", TotalTime = DateTime.MinValue, CurrentTime = DateTime.Now });
+            tasks.Add(new Task { NameTask = "zadanie 3", TotalTime = DateTime.MinValue, CurrentTime = DateTime.Now });
+
+            myComboBov.ItemsSource = tasks;
+        }
+
+       
+       
+
+        private void StartBT_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"tekst : { ZadanieTB.Text}");
+        }
+    }
+   
+}
